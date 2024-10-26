e0ed32edfa81fb83e7644f5158d03dc7372fa1b5
Raghava <Raghavendrarao.siraparapu@gmail.com>
2024-10-26 12:20:10 +0530

    changes

--- Ec2/dev/main.tf
+++ Ec2/dev/main.tf
@@ -4,7 +4,7 @@ module "vp_1" {
 
 module "ec2" {
     source = "./ins"
-    sg_id = module.sg3.sg3    
-    subnet_id = module.subnet1.subnet1
-    key_name = module.tls_private_key.tls_private_key
+    sg_id = module.sg3.sg_id 
+    subnet_id = module.subnet1.subnet1_id
+    key_name = module.tls_private_key.tls_private_keyss
     }
